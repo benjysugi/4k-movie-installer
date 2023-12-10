a0ce92808e94e8535c6efeef8807a603e823824d
NomNom <97398234+NomNom17@users.noreply.github.com>
2023-12-10 18:12:00 +0000

    Initial commit that does nothing yet

---
+++ Remastered FMVs Installer/ExternalFunctions.cs
@@ -0,0 +1,30 @@
+﻿using Spectre.Console;
+
+namespace Remastered_FMVs_Installer
+{
+    internal class ExternalFunctions
+    {
+        public static string UserTextColour = null;
+
+        public static void Options()
+        {
+            void chooseTextColour()
+            {
+
+            }
+        }
+
+        public static void CreateMenuPrompt(string HeadingText, string[] Choices, bool CreateHeading, bool CentreHeading)
+        {
+            if (CreateHeading)
+            {
+                CreateHeader(HeadingText, CentreHeading);
+            }
+        }
+
+        public static void CreateHeader(string HeadingText, bool CentreHeading)
+        {
+
+        }
+    }
+}
---
+++ Remastered FMVs Installer/Installer_Main.cs
@@ -0,0 +1,12 @@
+﻿using Spectre.Console;
+
+namespace Remastered_FMVs_Installer
+{
+    internal class Installer_Main
+    {
+        static void Main()
+        {
+            
+        }
+    }
+}
