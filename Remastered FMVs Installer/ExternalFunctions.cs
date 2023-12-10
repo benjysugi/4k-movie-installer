1d3db410d034f28d3ad513d2cb489a0e3c5579ff
NomNom <97398234+NomNom17@users.noreply.github.com>
2023-12-10 21:22:01 +0000

    Added drive info, game exe name checks, and beautified the console.

--- Remastered FMVs Installer/ExternalFunctions.cs
+++ Remastered FMVs Installer/ExternalFunctions.cs
@@ -1,17 +1,69 @@
 ﻿using Spectre.Console;
+using System.Diagnostics;
 
 namespace Remastered_FMVs_Installer
 {
     internal class ExternalFunctions
     {
-        public static string UserTextColour = null;
+        public static string UserTextColour = "blue";
+        public static string UserChoice = "";
 
         public static void Options()
         {
             void chooseTextColour()
             {
+                CreateMenuPrompt("Choose a text colour:", new string[] { "Red", "Green", "Blue", "Yellow", "Cyan", "Magenta", "White", "Black" }, true, true);
 
+                switch (UserChoice)
+                {
+                    case "Red":
+                        UserTextColour = "red";
+                        break;
+
+                    case "Green":
+                        UserTextColour = "green";
+                        break;
+
+                    case "Blue":
+                        UserTextColour = "blue";
+                        break;
+
+                    case "Yellow":
+                        UserTextColour = "yellow";
+                        break;
+
+                    case "Cyan":
+                        UserTextColour = "cyan";
+                        break;
+
+                    case "Magenta":
+                        UserTextColour = "magenta";
+                        break;
+
+                    case "White":
+                        UserTextColour = "white";
+                        break;
+
+                    case "Black":
+                        UserTextColour = "black";
+                        break;
+
+                    default:
+                        UserTextColour = "white";
+                        break;
+                }
             }
+        }
+
+        public static void GameList()
+        {
+
+        }
+
+        public static void ContinuePrompt()
+        {
+            AnsiConsole.MarkupLine("[grey][italic](Press any key to continue.)[/][/]");
+            Console.ReadKey();
         }
 
         public static void CreateMenuPrompt(string HeadingText, string[] Choices, bool CreateHeading, bool CentreHeading)
@@ -20,11 +72,113 @@ namespace Remastered_FMVs_Installer
             {
                 CreateHeader(HeadingText, CentreHeading);
             }
+
+            string userSelection = AnsiConsole.Prompt(new SelectionPrompt<string>()
+            .PageSize(4)
+            .AddChoices(Choices)
+            .MoreChoicesText("[grey][italic](Move up or down to reveal more options.)[/][/]")
+            .HighlightStyle(Style.WithDecoration(Decoration.RapidBlink).Foreground(Spectre.Console.Color.Blue))
+            );
+
+            UserChoice = userSelection;
         }
 
         public static void CreateHeader(string HeadingText, bool CentreHeading)
         {
+            Rule header = new();
 
+            if (CentreHeading) { header.Centered(); }
+            else { header.LeftJustified(); }
+
+            header.DoubleBorder();
+            header.Title = HeadingText;
+            header.Style = Style.Parse($"{UserTextColour}");
+            AnsiConsole.Write(header);
+        }
+
+        public static void CreateGameInfoTable(string[] RowInfo)
+        {
+            var gameInfoTable = new Table();
+
+            gameInfoTable.AddColumn("[green]Game Name[/]");
+            gameInfoTable.Columns[0].Centered();
+            gameInfoTable.AddColumn("[green]Developed by[/]");
+            gameInfoTable.Columns[1].Centered();
+            gameInfoTable.AddColumn("[green]Released on[/]");
+            gameInfoTable.Columns[2].Centered();
+            gameInfoTable.AddColumn("[green]Game Engine Used[/]");
+            gameInfoTable.Columns[3].Centered();
+            gameInfoTable.Border(TableBorder.Heavy);
+            gameInfoTable.DoubleBorder();
+            gameInfoTable.BorderStyle(new Style(Spectre.Console.Color.Blue));
+            gameInfoTable.Centered();
+            gameInfoTable.Title("Game information", Style.Parse($"{UserTextColour}"));
+
+            gameInfoTable.AddRow(RowInfo);
+
+            AnsiConsole.Write(gameInfoTable);
+        }
+
+        public static void DisplayStorageDriveInfo()
+        {
+            static DriveInfo[] GetDrivesInfo()
+            {
+                DriveInfo[] drives = DriveInfo.GetDrives();
+
+                return drives;
+            }
+
+            static string FormatBytes(long bytes)
+            {
+                const int scale = 1024; // 1024 bytes in a kilobyte etc.
+                string[] orders = { "TB", "GB", "MB", "KB", "Bytes" }; // the order of storage units
+                long max = (long)Math.Pow(scale, orders.Length - 1);
+
+                foreach (string order in orders)
+                {
+                    if (bytes > max)
+                        return string.Format("{0:##.##} {1}", decimal.Divide(bytes, max), order);
+
+                    max /= scale;
+                }
+
+                return "0 Bytes";
+            }
+
+            var driveTable = new Table();
+
+            driveTable.AddColumn("[green]Drive Letter[/]");
+            driveTable.Columns[0].Centered();
+            driveTable.AddColumn("[green]Drive Name[/]");
+            driveTable.Columns[1].Centered();
+            driveTable.AddColumn("[green]Drive Type[/]");
+            driveTable.Columns[2].Centered();
+            driveTable.AddColumn("[green]Format Type[/]");
+            driveTable.Columns[3].Centered();
+            driveTable.AddColumn("[green]Free Space[/]");
+            driveTable.Columns[4].Centered();
+            driveTable.AddColumn("[green]Total Space[/]");
+            driveTable.Columns[5].Centered();
+            driveTable.Border(TableBorder.Heavy);
+            driveTable.DoubleBorder();
+            driveTable.BorderStyle(new Style(Spectre.Console.Color.Blue));
+            driveTable.Centered();
+            driveTable.Title("Storage Devices", Style.Parse($"{UserTextColour}"));
+
+            foreach (DriveInfo drive in GetDrivesInfo())
+            {
+                driveTable.AddRow(drive.Name, drive.VolumeLabel, drive.DriveFormat, drive.DriveType.ToString(), FormatBytes(drive.AvailableFreeSpace), FormatBytes(drive.TotalSize));
+            }
+
+            AnsiConsole.WriteLine();
+            AnsiConsole.Write(driveTable);
+        }
+
+        public static bool IsTerminalProcessRunning(string processName)
+        {
+            Process[] processes = Process.GetProcessesByName(processName);
+
+            return processes.Length > 0;
         }
     }
 }
--- Remastered FMVs Installer/Installer_Main.cs
+++ Remastered FMVs Installer/Installer_Main.cs
@@ -1,12 +1,103 @@
 ﻿using Spectre.Console;
+using System.Diagnostics;
+using System.Windows.Forms;
 
 namespace Remastered_FMVs_Installer
 {
     internal class Installer_Main
     {
-        static void Main()
+        [STAThread]
+        public static void Main()
         {
-            
+            AnsiConsole.Write(new FigletText("Remastered FMVs Installer").Color(Spectre.Console.Color.Blue).Centered());
+            AnsiConsole.Write(new Markup($"[bold][underline][{ExternalFunctions.UserTextColour}]Created by: NomNom[/][/][/]\n" +
+                $"[{ExternalFunctions.UserTextColour}]Join the [link=https://discord.gg/KnsDNgFm2V]Discord Server.[/][/]\n" +
+                $"[italic][grey]CTRL + Left Click to open the link.[/][/]").Centered());
+            AnsiConsole.Write(new Markup($"[{ExternalFunctions.UserTextColour}]\nWelcome to the Remastered FMVs Installer![/]").Centered());
+            ExternalFunctions.CreateHeader("Version: 0.0.1 - PreAlpha", true);
+
+            ExternalFunctions.CreateMenuPrompt("What would you like to do?", new[] { "Install the 4K FMVs", "Uninstall the 4K FMVs", "About", "More", "Exit" }, false, false);
+
+            switch (ExternalFunctions.UserChoice)
+            {
+                case "Install the 4K FMVs":
+                    bool userQuickSFVPrompt = false;
+
+                    while (userQuickSFVPrompt == false)
+                    {
+                        ExternalFunctions.CreateMenuPrompt("Would you like to check the file's integrity by using QuickSFV?", new[] { "Yes", "No" }, true, true);
+
+                        switch (ExternalFunctions.UserChoice)
+                        {
+                            case "Yes":
+                                Process.Start("Data\\QuickSFV\\QuickSFV.EXE");
+                                userQuickSFVPrompt = true;
+                                break;
+
+                            case "No":
+                                InstallFMVs();
+                                userQuickSFVPrompt = true;
+                                break;
+                        }
+                    }
+                    break;
+            }
+        }
+
+        static void InstallFMVs()
+        {
+            void GetGameInformation(string ExecutableName)
+            {
+                switch (ExecutableName)
+                {
+                    case "Bully Scholarship Edition.exe":
+
+                        ExternalFunctions.CreateGameInfoTable(new[] { "Bully: Scholarship Edition", "Rockstar North", "21st of October 2008", "RAGE Engine" });
+
+                        AnsiConsole.MarkupLine("Game: Bully: Scholarship Edition (2005)");
+                        break;
+
+                    default:
+                        AnsiConsole.MarkupLine($"[red]Unknown game selected: {ExecutableName}.[/]\n[{ExternalFunctions.UserTextColour}]If this game's FMVs has already been remastered, but the automatic installation is not supported by this installer, most likely I have either forgotton, or, is currently being worked on.[/]");
+                        break;
+                }
+            }
+
+            string processName = "";
+            string folderPath = "";
+
+            AnsiConsole.MarkupLine("[red]PLEASE NOTE; if you have external HDDs connected to your PC, Windows 10 and 11 will wake these drives up from sleep. This may cause this app to freeze until the drives have woken up.[/]\n");
+
+            ExternalFunctions.DisplayStorageDriveInfo();
+
+            AnsiConsole.MarkupLine($"[{ExternalFunctions.UserTextColour}]Please locate the game's executable. After you press any key to continue; a window will appear, asking you to open the game's executable file.[/]");
+
+            ExternalFunctions.ContinuePrompt();
+
+            AnsiConsole.WriteLine();
+
+            using OpenFileDialog openExecutableDialog = new OpenFileDialog();
+            openExecutableDialog.Filter = "Game's Exectuable (*.exe)|*.exe";
+            openExecutableDialog.Title = "Locate the game's executable";
+
+            if (openExecutableDialog.ShowDialog() == DialogResult.OK)
+            {
+                // Get the selected file path
+                string selectedExecutablePath = openExecutableDialog.FileName;
+
+                // Get the folder path from the selected file path
+                string gameFolderPath = Path.GetDirectoryName(selectedExecutablePath);
+
+                // Get the selected file path
+                string selectedExecutableName = Path.GetFileName(gameFolderPath);
+
+                selectedExecutableName += ".exe";
+
+                GetGameInformation(selectedExecutableName);
+
+                AnsiConsole.WriteLine($"Selected executable: {selectedExecutableName}");
+                AnsiConsole.WriteLine($"Folder path: {gameFolderPath}");
+            }
         }
     }
 }
